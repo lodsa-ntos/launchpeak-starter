fa8ddab6f20c7480cfa63def3e6f0fa3620abac0
Lodney Santos <a21505293@alunos.ulht.pt>
2025-05-04 21:56:46 +0100

    Remove "Proud partners" section from CompanyLogo and enhance CSS reset rules

--- src/components/CompanyLogo.jsx
+++ src/components/CompanyLogo.jsx
@@ -23,10 +23,6 @@ function CompanyLogo(){
   return (
     <div className='w-full overflow-hidden container mx-auto py-20 gap-8 flex sm:flex-row flex-col sm:items-center items-start'>
 
-      <div className='w-[300px] shrink-0 px-5 text-gray-600 border-l-4 border-blue-500 bg-white py-2 z-10 sm:text-base text-xl font-semibold font-Satoshi text-left'>
-        Proud partners
-      </div>
-
       <div className='flex whitespace-nowrap animate-walkingX'>
         {companies.map((company, index) => (
           <img key={index} src={company} alt="company logo" className='mx-12 h-8 w-36 object-contain grayscale opacity-75 hover:grayscale-0 hover:opacity-100 transition-all'/>
