7a258c5e89d4a892142ebb9eb038c07854f1cf0e
Lodney Santos <a21505293@alunos.ulht.pt>
2025-05-04 07:00:44 +0100

    Update CompanyLogo component layout and content

--- src/components/CompanyLogo.jsx
+++ src/components/CompanyLogo.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function CompanyLogo(){
   return (
-    <div className='w-full container mx-auto py-20 flex'>CompanyLogo</div>
+    <div className='w-full container mx-auto py-20 px-4 sm:px- lg:px-8 flex sm:flex-row flex-col sm:items-center items-start'>
+        <div>
+            Proud Partners
+        </div>
+    </div>
   )
 }
 
