5c1ee56349f31bd6cdf0ea1a59e0734cd61684d6
Lodney Santos <a21505293@alunos.ulht.pt>
2025-05-04 21:29:32 +0100

    Add id attribute to HeroSection and enhance scroll behavior in CSS

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ import { PiRocketLaunchFill } from "react-icons/pi"
 function HeroSection() {
 
   return (
-    <section className="container mx-auto flex flex-col md:flex-row justify-between items-center pt-44 pb-6 px-4 sm:px-6 lg:px-8">
+    <section id='home' className="container mx-auto flex flex-col md:flex-row justify-between items-center pt-44 pb-6 px-4 sm:px-6 lg:px-8">
 
         {/* Hero Section - Left Col */}
         <div className='w-full md:w-1/2 space-y-8'>
