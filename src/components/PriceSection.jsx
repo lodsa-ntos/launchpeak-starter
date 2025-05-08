45ed9e49d04dc6a3be35cdb56d7b07f0502c1e32
Lodney Santos <a21505293@alunos.ulht.pt>
2025-05-08 19:15:03 +0100

    Implement PriceSection layout with structured pricing details; add ContactSection and Footer components for improved site structure

--- src/components/PriceSection.jsx
+++ src/components/PriceSection.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 function PriceSection() {
   return (
     <section>
-        <div>PriceSection</div>
+        <div className='container mx-auto py-20'>
+            <div className="text-center">
+                <div className="title-span">Pricing</div>
+                <h2 className='title'>My Pricing Plan</h2>
+            </div>
+
+            <div className='row-auto mt-5'>
+                <div className="col-md-4">
+                    <div className="pricing-box">
+                        <div className="price-inner">
+                            <div className="pr-tag">Basics</div>
+                            <h5><span>120</span></h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
   )
 }
