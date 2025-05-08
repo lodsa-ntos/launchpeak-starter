468756d01b31c07f26187845c31665ff1f45aab9
Lodney Santos <a21505293@alunos.ulht.pt>
2025-05-08 19:06:50 +0100

    Enhance ExempleSection layout with icon integration and improved styling for better visual appeal

--- src/components/ExempleSection.jsx
+++ src/components/ExempleSection.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import imgCard2 from '../assets/usecases/analysis.jpg'
+import { FaReact } from "react-icons/fa"
+import { RiTailwindCssFill } from "react-icons/ri"
+import { DiResponsive } from "react-icons/di"
+import { PiDeviceMobileCameraLight } from "react-icons/pi"
+import { TbBrandFramerMotion } from "react-icons/tb"
 
 function ExempleSection() {
 
@@ -23,14 +28,19 @@ function ExempleSection() {
                   <div className="overflow-hidden relative rounded-md w-[20rem]">
                     <img className='transform hover:scale-[1.2]' src={imgCard2} alt="examples images" />
                   </div>
-                  <div className="bg-lp-purple text-white text-xs left-3 top-3 absolute px-3 py-1 rounded-full font-medium shadow-sm">LaunchPeack Edu</div>
+                  <div className="left-3 top-3 absolute px-2 py-1 rounded-fullshadow-sm inline-flex items-center rounded-md bg-purple-50 text-xs font-medium text-purple-700 ring-1 ring-purple-700/10 ring-inset">LaunchPeack Edu</div>
                 </div>
                 <div className="pt-[20px]">
                   <div className="flex items-center gap-[12px] mr-[10px]">
-                    <a className='text-lp-purple-light text-sm font-[600] font-Satoshi' href="">08 April 2025</a>
-                    <a className='text-lp-purple-light text-sm font-[600] font-Satoshi' href="">By coding Lod Santos</a>
+                    <span className='items-center justify-between gap-1 flex'>
+                      <a className='flex flex-wrap items-center gap-1.5 rounded-full bg-blue-50 px-2.5 py-1 text-xs font-medium text-blue-600 ring-1 ring-blue-700/10 ring-inset font-Satoshi tracking-tight' href="https://react.dev/"><FaReact /> React.js</a>
+
+                      <a className='flex flex-wrap items-center gap-1.5 rounded-full bg-cyan-50 px-2.5 py-1 text-xs font-medium text-cyan-600 ring-1 ring-blue-700/10 ring-inset font-Satoshi tracking-tight' href="https://tailwindcss.com/"><RiTailwindCssFill /> TailwindCSS</a>
+
+                      <a className='flex flex-wrap items-center gap-1.5 rounded-full bg-purple-50-50 px-2.5 py-1 text-xs font-medium text-purple-600 ring-1 ring-blue-700/10 ring-inset font-Satoshi tracking-tight' href="https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/CSS_layout/Responsive_Design"><DiResponsive /> Responsive</a>
+                    </span>
                   </div>
-                  <h5 className='text-base font-bold font-Satoshi'>Launch your course with clarity and style</h5>
+                  <h5 className='text-base font-bold font-Satoshi mt-2'>Launch your course with clarity and style</h5>
                   <p className='font-Satoshi'>Boost your online course signups</p>
                 </div>
               </div>
@@ -43,14 +53,19 @@ function ExempleSection() {
                   <div className="overflow-hidden relative rounded-md w-[20rem]">
                     <img className='transform hover:scale-[1.2]' src={imgCard2} alt="examples images" />
                   </div>
-                  <div className="bg-lp-purple text-white text-xs left-3 top-3 absolute px-3 py-1 rounded-full font-medium shadow-sm">LaunchPeack App</div>
+                  <div className="left-3 top-3 absolute px-2 py-1 rounded-fullshadow-sm inline-flex items-center rounded-md bg-purple-50 text-xs font-medium text-purple-700 ring-1 ring-purple-700/10 ring-inset">LaunchPeack App</div>
                 </div>
                 <div className="pt-[20px]">
                   <div className="flex items-center gap-[12px] mr-[10px]">
-                    <a className='text-lp-purple-light text-sm font-[600] font-Satoshi' href="">08 April 2025</a>
-                    <a className='text-lp-purple-light text-sm font-[600] font-Satoshi' href="">By coding Lod Santos</a>
+                  <span className='items-center justify-between gap-1 flex'>
+                      <a className='flex flex-wrap items-center gap-1.5 rounded-full bg-blue-50 px-2.5 py-1 text-xs font-medium text-blue-600 ring-1 ring-blue-700/10 ring-inset font-Satoshi tracking-tight' href="https://react.dev/"><FaReact /> React.js</a>
+
+                      <a className='flex flex-wrap items-center gap-1.5 rounded-full bg-cyan-50 px-2.5 py-1 text-xs font-medium text-cyan-600 ring-1 ring-blue-700/10 ring-inset font-Satoshi tracking-tight' href="https://www.uxpin.com/studio/blog/a-hands-on-guide-to-mobile-first-design/"><PiDeviceMobileCameraLight /> Mobile-First UI</a>
+
+                      <a className='flex flex-wrap items-center gap-1.5 rounded-full bg-purple-50-50 px-2.5 py-1 text-xs font-medium text-purple-600 ring-1 ring-blue-700/10 ring-inset font-Satoshi tracking-tight' href="https://motion.dev/"><TbBrandFramerMotion /> Frame Motion</a>
+                    </span>
                   </div>
-                  <h5 className='text-base font-bold font-Satoshi'>Convert users with a high-impact landing</h5>
+                  <h5 className='text-base font-bold font-Satoshi mt-2'>Convert users with a high-impact landing</h5>
                   <p className='font-Satoshi'>Landing page that converts app installs.</p>
                 </div>
               </div>
@@ -63,14 +78,19 @@ function ExempleSection() {
                   <div className="overflow-hidden relative rounded-md w-[20rem]">
                     <img className='transform hover:scale-[1.2]' src={imgCard2} alt="examples images" />
                   </div>
-                  <div className="bg-lp-purple text-white text-xs left-3 top-3 absolute px-3 py-1 rounded-full font-medium shadow-sm">LaunchPeak Portfolio</div>
+                  <div className="left-3 top-3 absolute px-2 py-1 rounded-fullshadow-sm inline-flex items-center rounded-md bg-purple-50 text-xs font-medium text-purple-700 ring-1 ring-purple-700/10 ring-inset">LaunchPeak Portfolio</div>
                 </div>
                 <div className="pt-[20px]">
                   <div className="flex items-center gap-[12px] mr-[10px]">
-                    <a className='text-lp-purple-light text-sm font-[600] font-Satoshi' href="">08 April 2025</a>
-                    <a className='text-lp-purple-light text-sm font-[600] font-Satoshi' href="">By coding Lod Santos</a>
+                  <span className='items-center justify-between gap-1 flex'>
+                      <a className='flex flex-wrap items-center gap-1.5 rounded-full bg-blue-50 px-2.5 py-1 text-xs font-medium text-blue-600 ring-1 ring-blue-700/10 ring-inset font-Satoshi tracking-tight' href="https://react.dev/"><FaReact /> React.js</a>
+
+                      <a className='flex flex-wrap items-center gap-1.5 rounded-full bg-cyan-50 px-2.5 py-1 text-xs font-medium text-cyan-600 ring-1 ring-blue-700/10 ring-inset font-Satoshi tracking-tight' href="https://tailwindcss.com/"><RiTailwindCssFill /> TailwindCSS</a>
+
+                      <a className='flex flex-wrap items-center gap-1.5 rounded-full bg-purple-50-50 px-2.5 py-1 text-xs font-medium text-purple-600 ring-1 ring-blue-700/10 ring-inset font-Satoshi tracking-tight' href="https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/CSS_layout/Responsive_Design"><DiResponsive /> Responsive</a>
+                    </span>
                   </div>
-                  <h5 className='text-base font-bold font-Satoshi'>Stand out with a clean and modern</h5>
+                  <h5 className='text-base font-bold font-Satoshi mt-2'>Stand out with a clean and modern</h5>
                   <p className='font-Satoshi'>Present your work with clarity and impact.</p>
                 </div>
               </div>
